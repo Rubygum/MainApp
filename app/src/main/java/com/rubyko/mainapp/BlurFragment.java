d434ba484de7f902f35496271444a2eb2dfda57d
YehorJava <yegor.java@gmail.com>
2016-02-18 15:24:42 +0800

    Added blur fragment. Reorgazed splash screen.

--- app/src/main/java/com/rubyko/mainapp/BlurFragment.java
+++ app/src/main/java/com/rubyko/mainapp/BlurFragment.java
@@ -1,42 +1,7 @@
 package com.rubyko.mainapp;
 
-import android.os.Bundle;
-
-import fr.tvbarthel.lib.blurdialogfragment.BlurDialogEngine;
-
 /**
  * Created by yegor on 18/02/16.
  */
-public class BlurFragment<T extends  BaseActivity> extends BaseFragment<T> {
-
-    /**
-     * Engine used to blur.
-     */
-    private BlurDialogEngine mBlurEngine;
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mBlurEngine = new BlurDialogEngine(getActivity());
-        mBlurEngine.setBlurRadius(8);
-        mBlurEngine.setDownScaleFactor(8f);
-        mBlurEngine.debug(false);
-        mBlurEngine.setBlurActionBar(true);
-        mBlurEngine.setUseRenderScript(true);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        mBlurEngine.onResume(getRetainInstance());
-    }
-
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mBlurEngine.onDestroy();
-    }
-
+public class BlurFragment {
 }
